40c4a8a7dc2f2e198db3c50b4489632bb792f6ab
Alumno IDS <alumnoids@iMac-de-IT-Admin.local>
2018-02-15 13:54:01 -0600

    Tarea4-5:Date Logic_1

--- Tareas/Tarea4_5/Tarea4_5.playground/Contents.swift
+++ Tareas/Tarea4_5/Tarea4_5.playground/Contents.swift
@@ -21,14 +21,40 @@ import UIKit
 // UTC, GMT
 //Format("YYYY/mm/dd:T:HH:MM:SS:mm.000'Z'") - hulu format date
 import UIKit
-let FORMAT = "yy-MM-dd"
+let FORMAT:String = "yyyy-MM-dd"
 var now = Date()  //MM d, YYYY 'at' hh:mm 'PM'var pastDate = Date(timeIntervalSinceNow: -122 )
 // String x = "";// x.reverse()// x.charAt(index)
 
-//
+/*
+ Extencion Date
+ 
+ - Funcion getTimeAgo():
+    
+    * Se agregara la funcion de obtener el tiempo que ha pasado desde el momento dado hasta
+      el momento actual (timestamp), mandado un formato de salido dependiendo del tiempo transcurrido:
+        - Menos de un minuto: "Segundos transcurridos¨ seconds ago.
+        - Menos de una hora: "Minutos transcurridos¨ minutos ago.
+        - ... Hasta llegar a meses
+        - Llegando al año se mostrara la fecha.
+    * Regresara un tipo de valor String, cadena de caracteres con el formato por default 
+      "yyyy-MM-dd", Ejemplo: 2018-01-30
+    * Dependecias:
+        - FormatDate()
+ 
+ -Funtion formatDate():
+    
+    *   Se agrego la funcion para darle un formato a la fecha segun un String, una cadena de caracteres, que es
+        que es un patron como el siguiente ejemplo: "yy-MM-dd" otro ejemplo "EEE, d MMM yyyy HH:mm:ss Z"
+    *   Los parametros aceptados son 
+ 
+ 
+ 
+ */
+
+
 extension Date {
     
-    func getTimeAgo() -> String {
+    func getTimeAgo(formatPatterForTimeAgo:String?) -> String {
         
         let timeAgo = Int(Date().timeIntervalSince(self))
         
@@ -47,26 +73,33 @@ extension Date {
         }
             //before a month print the format yyyy/mm/dd - extra
         else if timeAgo < 60 * 60 * 24 * 7 * 4 * 12 {
-            return "\(self.formatDate(formatPattern: FORMAT)) , \(timeAgo / 60 / 60 / 24 / 7 / 4) months ago"
+            return "\(self.formatDate(givenFormatPattern: formatPatterForTimeAgo))) , \(timeAgo / 60 / 60 / 24 / 7 / 4) months ago"
         }
-        return "Post on \(self.formatDate(formatPattern: FORMAT))"
+        return "Post on \(self.formatDate(givenFormatPattern: formatPatterForTimeAgo))"
     }
     
+    
+    
+    
+    
     //Function to format  dates
-    func formatDate(formatPattern:String) -> String {
+    func formatDate(givenFormatPattern:String? = nil) -> String {
         //Creation of the dateformmater
         let dateFormatter:DateFormatter = DateFormatter() //La ide me dijo que es let :v
-        //Set the date format
+        var formatPattern:String = (givenFormatPattern != nil ? givenFormatPattern! : FORMAT)
         dateFormatter.dateFormat = formatPattern
         //Convetion of the date to string
         let formattedDate:String = dateFormatter.string(from: self)//La ide me dijo que es let :v
         return formattedDate
     }
-    
-    //Formmat Date @params (Date(),Format)
+
 }
-var tiempo = Date(timeIntervalSinceNow: -60*60*24*7*4*11).getTimeAgo()
-print(tiempo)
+
+var date = Date().formatDate()
+//Tests
+
+//var tiempo = Date(timeIntervalSinceNow: -60*60*24*7*4*11).getTimeAgo(formatPatterForTimeAgo: nil)
+//print(tiempo)
 
 
 //var secondsAgo: String = pastDate.getTimeAgo()
